61d84803d0e7c94e9a5802abb5587cd3d33f1cfe
VaibhavDhamanage <vaibhavdhamanage>
2024-02-08 12:45:45 +0530

    change Routing from using Outlet to using Router And Route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Header from "./Component/Header/Header";
-import Main from "./Component/Main/main";
-import Footer from "./Component/Footer/Footer";
-import Cart from "./Component/Cart/Cart";
+import Layout from "./Component/Pages/Layout";
+import StorePage from "./Component/Pages/Store";
+import About from "./Component/Pages/About";
+import Login from "./Component/Login/LoginForm";
+import AuthContext from "./Store/Auth-Context";
+import Home from "./Component/Pages/Home";
+import Contact from "./Component/Pages/ContactUs";
 
 function App() {
+  const context = useContext(AuthContext);
+
   return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
+    <Layout>
+      <main>
+        <Routes>
+          {!context.isLoggedIn && <Route path="/login" element={<Login />} />}
+          {context.isLoggedIn && <Route path="/" element={<Home />} />}
+          {context.isLoggedIn && (
+            <Route path="/Store" element={<StorePage />} />
+          )}
+          {context.isLoggedIn && <Route path="/About" element={<About />} />}
+          {context.isLoggedIn && (
+            <Route path="/Contact" element={<Contact />} />
+          )}
+          {!context.isLoggedIn && <Route path="*" element={<Login />} />}
+        </Routes>
+      </main>
+    </Layout>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,56 +1,20 @@
-import React, { useContext } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "../node_modules/react-bootstrap/dist/react-bootstrap";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import ContextProvider from "./Store/ContextProvider";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Layout from "./Component/Pages/Layout";
-import StorePage from "./Component/Pages/Store";
-import About from "./Component/Pages/About";
-import Home from "./Component/Pages/Home";
-import Contact from "./Component/Pages/ContactUs";
-import ProductDetails from "./Component/Pages/ProductDetails";
-import Login from "./Component/Login/LoginForm";
+import { BrowserRouter } from "react-router-dom";
 import AuthContextProvider from "./Store/AuthContextProvider";
-import AuthContext from "./Store/Auth-Context";
-
-const router = createBrowserRouter([
-  {
-    path: "",
-    element: <Layout />,
-    children: [
-      { path: "/", element: <Home /> },
-      {
-        path: "/Store",
-        element: <StorePage />,
-      },
-      {
-        path: "/Store/:productId",
-        element: <ProductDetails />,
-      },
-      {
-        path: "/About",
-        element: <About />,
-      },
-      {
-        path: "/Contact",
-        element: <Contact />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-    ],
-  },
-]);
-// const context = useContext(AuthContext);
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthContextProvider>
     <ContextProvider>
-      <RouterProvider router={router}></RouterProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ContextProvider>
   </AuthContextProvider>
 );
