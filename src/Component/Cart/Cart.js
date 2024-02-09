5f727d5a43f363e98db21353b21193929a897d0b
VaibhavDhamanage <vaibhavdhamanage>
2024-02-09 16:42:14 +0530

    fetch GET request

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -1,32 +1,24 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import ItemCart from "./ItemCart";
 import Modal from "../UI/Modal";
 import ContextStore from "../../Store/Context";
-
-const cartElements = [
-  {
-    title: "Colors",
-    price: 100,
-    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
-    quantity: 2,
-  },
-  {
-    title: "Black and white Colors",
-    price: 50,
-    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
-    quantity: 3,
-  },
-  {
-    title: "Yellow and Black Colors",
-    price: 70,
-    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
-    quantity: 1,
-  },
-];
+import AuthContext from "../../Store/Auth-Context";
 
 const Cart = (props) => {
   const context = useContext(ContextStore);
+  const Auth_Context = useContext(AuthContext);
+  const [cart, setcart] = useState([]);
 
+  useEffect(() => {
+    fetch(
+      "https://crudcrud.com/api/19bffb556e4d4c3180cc77d051f5deec/" +
+        Auth_Context.Email
+    )
+      .then((res) => res.json())
+      .then((data) => (setcart(data), context.AddItems(data)));
+  }, []);
+
+  console.log(Auth_Context.Email);
   const order = () => {
     alert("Your Purchase Order Successfully !");
     context.Order();
@@ -40,7 +32,7 @@ const Cart = (props) => {
             <div class="p-2">
               <h4>Shopping cart</h4>
             </div>
-            {context.CartItems.map((item) => (
+            {cart.map((item) => (
               <ItemCart item={item} />
             ))}
             <div class="d-flex flex-row align-items-center mt-3 p-2 bg-white rounded gap-3 ">
