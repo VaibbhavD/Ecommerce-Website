664a8e365fd0d37dc3dbf30fb32eb22fc4e9ad8e
VaibhavDhamanage <vaibhavdhamanage>
2024-02-09 16:44:29 +0530

    Add Email and initilization

--- src/Store/AuthContextProvider.js
+++ src/Store/AuthContextProvider.js
@@ -3,22 +3,30 @@ import AuthContext from "./Auth-Context";
 
 const AuthContextProvider = (props) => {
   const InitialToken = localStorage.getItem("token");
+  const InitialEmail = localStorage.getItem("email");
   const [token, settoekn] = useState(InitialToken);
+  const [email, setemail] = useState(InitialEmail);
 
   const isLoggedIn = !!token;
 
-  const Login = (token) => {
+  const Login = (token, Email) => {
     localStorage.setItem("token", token);
+    let EMail = Email.replaceAll(".", "");
+    EMail = EMail.replaceAll("@", "");
+    setemail(EMail);
+    localStorage.setItem("email", EMail);
     settoekn(token);
   };
 
   const Logout = () => {
     localStorage.removeItem("token");
+    localStorage.removeItem("email");
     settoekn(null);
   };
 
   const context = {
     token: token,
+    Email: email,
     isLoggedIn: isLoggedIn,
     Login: Login,
     Logout: Logout,
