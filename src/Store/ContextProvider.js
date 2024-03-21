f20df2de9937b6a49807aea8fc4dfe6c590af80c
VaibhavDhamanage <vaibhavdhamanage>
2024-03-21 12:48:45 +0530

    Products take from external api and save context state

--- src/Store/ContextProvider.js
+++ src/Store/ContextProvider.js
@@ -1,44 +1,44 @@
 import React, { useContext, useEffect, useState } from "react";
 import ContextStore from "./Context";
-import AuthContext from "./Auth-Context";
 
-const Products = [
-  {
-    title: "Album 1",
-    Des: "Colors",
-    price: 100,
-    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
-    id: "a1",
-  },
-  {
-    title: "Album 2",
-    Des: "Black and white Colors",
-    price: 50,
-    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
-    id: "a2",
-  },
-  {
-    title: "Album 3",
-    Des: "Yellow and Black Colors",
-    price: 70,
-    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
-    id: "a3",
-  },
-  {
-    title: "Album 4",
-    Des: "Blue Color",
-    price: 100,
-    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
-    id: "a4",
-  },
-];
+// const Products = [
+//   {
+//     title: "Album 1",
+//     Des: "Colors",
+//     price: 100,
+//     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+//     id: "a1",
+//   },
+//   {
+//     title: "Album 2",
+//     Des: "Black and white Colors",
+//     price: 50,
+//     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+//     id: "a2",
+//   },
+//   {
+//     title: "Album 3",
+//     Des: "Yellow and Black Colors",
+//     price: 70,
+//     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+//     id: "a3",
+//   },
+//   {
+//     title: "Album 4",
+//     Des: "Blue Color",
+//     price: 100,
+//     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
+//     id: "a4",
+//   },
+// ];
 
 const ContextProvider = (props) => {
   const [Items, setItems] = useState([]);
-  const Auth_Context = useContext(AuthContext);
+  const [showcart, setshowcart] = useState(false);
+  const [Products, SetProducts] = useState([]);
 
   const AddItems = (item) => {
-    setItems(item);
+    setItems((prev) => [...prev, item]);
   };
 
   const RemoveItem = (item) => {
@@ -50,21 +50,47 @@ const ContextProvider = (props) => {
     setItems([]);
   };
 
-  useEffect(() => {
-    fetch(
-      "https://crudcrud.com/api/19bffb556e4d4c3180cc77d051f5deec/" +
-        Auth_Context.Email
-    )
-      .then((res) => res.json())
-      .then((data) => context.AddItems(data));
-  }, [Auth_Context.Login]);
+  const ShowCart = () => {
+    setshowcart((prev) => !prev);
+  };
+
+  const UpdateCart = (item) => {
+    setItems(item);
+  };
+
+  const updateProducts = (data) => {
+    SetProducts(data);
+  };
+
+  useEffect(async () => {
+    const fetchdata = async () => {
+      const res = await fetch("https://dummyjson.com/products");
+      if (!res) {
+        alert("Error Fetching Data");
+      }
+      const data = res.json();
+      return data;
+    };
+    try {
+      const data = await fetchdata();
+      // console.log(data.products);
+      SetProducts(data.products);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }, []);
 
   const context = {
     Products: Products,
     CartItems: Items,
     AddItems: AddItems,
     RemoveItem: RemoveItem,
+    UpdateCart,
+    UpdateCart,
     Order: Order,
+    Showcart: showcart,
+    enableShowCart: ShowCart,
+    UpdateProducts: updateProducts,
   };
 
   return (
