9c817f99023c5cb602ddfa16bfc1b207700b950d
VaibhavDhamanage <vaibhavdhamanage>
2024-03-21 12:50:32 +0530

    added dynamic traverse into Products

--- src/Component/Main/main.js
+++ src/Component/Main/main.js
@@ -1,31 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Button, Container } from "react-bootstrap";
 import ItemCart from "./ItemCart";
 import ContextStore from "../../Store/Context";
+import { GetProducts } from "../../Store/ProductsApi";
 
 const Main = () => {
   const context = useContext(ContextStore);
   const Products = context.Products;
 
   return (
-    <Container>
-      <div class="container text-center mt-5">
-        <div class="row g-2">
-          <div class="col-6">
-            <ItemCart item={Products[0]} />
-          </div>
-          <div class="col-6">
-            <ItemCart item={Products[1]} />
-          </div>
-          <div class="col-6">
-            <ItemCart item={Products[2]} />
-          </div>
-          <div class="col-6">
-            <ItemCart item={Products[3]} />
-          </div>
+    <section class="py-5">
+      <div class="container px-4 px-lg-5 mt-5">
+        <div class="row gx-5 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+          {Products.map((product) => (
+            <ItemCart item={product} />
+          ))}
         </div>
       </div>
-    </Container>
+    </section>
   );
 };
 export default Main;
