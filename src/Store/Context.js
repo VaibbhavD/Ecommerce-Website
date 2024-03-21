fa0ebd3e9282b7c82839e1307f967f74021ab5b9
VaibhavDhamanage <vaibhavdhamanage>
2024-03-21 12:49:01 +0530

    add functions and states

--- src/Store/Context.js
+++ src/Store/Context.js
@@ -3,9 +3,13 @@ import React from "react";
 const ContextStore = React.createContext({
   Products: [],
   CartItems: [],
+  Showcart: false,
   AddItems: (item) => {},
   RemoveItem: (item) => {},
   Order: () => {},
+  UpdateCart: () => {},
+  enableShowCart: () => {},
+  UpdateProducts: () => {},
 });
 
 export default ContextStore;
